e4265017f1b30d43109360838785b89160e1dada
Qasim Ziauddin <q.h.ziauddin@se15.qmul.ac.uk>
2018-11-01 19:35:44 +0000

    initialCommit
    initial commit

---
+++ Survive The Night/Assets/Game/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+    public float speed = 10.0f;
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start ()
+    {
+        speed = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            speed = 10.0f;
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+    }
+
+    
+}
