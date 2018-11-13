cef1e5f6caab44cc7e5c0c2db73ed344ba41ea76
Qasim Ziauddin <q.h.ziauddin@se15.qmul.ac.uk>
2018-11-13 00:45:33 +0000

    Fire bullets and destroy enemies
    Can now fire bullets and kill enemies

--- Survive The Night/Assets/Game/Scripts/PlayerControls.cs
+++ Survive The Night/Assets/Game/Scripts/PlayerControls.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
-    public float speed = 10.0f;
     private Vector3 target;
+    public float playerSpeed = 7f;
+    public GameObject bullet;
+    Rigidbody2D rb;
+    Vector2 bulletPos;
 
     // Use this for initialization
     void Start ()
     {
-        speed = 0f;
-	}
+        target = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            speed = 10.0f;
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.z = transform.position.z;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-
+        MoveCharacter();
     }
 
-    
+    void MoveCharacter()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            fireBullet();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, playerSpeed * Time.deltaTime);
+    }
+
+
+    void fireBullet()
+    {
+        bulletPos = transform.position;
+        bulletPos += new Vector2(1f, -0.5f);
+        Instantiate(bullet, bulletPos, Quaternion.identity);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        target = transform.position;
+    }
+
 }
