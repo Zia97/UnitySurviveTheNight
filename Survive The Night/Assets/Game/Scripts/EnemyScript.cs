4fdee46446af7f4aad2586c84123dbc15ddbbb62
Qasim Ziauddin <q.h.ziauddin@se15.qmul.ac.uk>
2018-12-22 20:43:08 +0000

    Basic repeating waves

--- Survive The Night/Assets/Game/Scripts/EnemyScript.cs
+++ Survive The Night/Assets/Game/Scripts/EnemyScript.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
 
     private float speed = 3.0f;
     private bool isMoving = true;
     private double health = 100;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (isMoving)
         {
             transform.position += -transform.right * speed * Time.deltaTime;
@@ -23,23 +26,27 @@ public class EnemyScript : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Hit:"+collision.collider.name);
-        if (collision.transform.gameObject.name != "wall")
-        {
-            if (collision.transform.gameObject.name == "Bullet" || collision.transform.gameObject.name == "Bullet(Clone)")
-            {
-                health = health - 30;
-            }
-            if (health <= 0)
-            {
-                Destroy(this.gameObject);
-            }
-          
-        }
-        else
+        Debug.Log("Hit:" + collision.collider.name);
+
+        
+        if (collision.transform.gameObject.name == "wall")
         {
             isMoving = false;
+        }
+
+        else if (collision.transform.gameObject.name == "Bullet" || collision.transform.gameObject.name == "Bullet(Clone)")
+        {
+            health = health - 30;
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
         
+        else
+        {
+            Debug.Log("Unidentified enemy collision");
         }
     }
 }
--- Survive The Night/Assets/Game/Scripts/GameController.cs
+++ Survive The Night/Assets/Game/Scripts/GameController.cs
@@ -1,19 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public Vector3 spawnValues;
 
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
     private void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
     }
 
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3(spawnValues.x,Random.Range(-spawnValues.y, spawnValues.y),spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y), spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
+      
     }
+
 }
