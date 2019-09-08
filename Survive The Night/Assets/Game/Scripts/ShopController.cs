e4e3a53ef2cce0b3ba9ec9d2b4298845801814a9
Zia97 <qasimziauddin@gmail.com>
2019-09-08 22:09:07 +0100

    shop work

--- Survive The Night/Assets/Game/Scripts/ShopController.cs
+++ Survive The Night/Assets/Game/Scripts/ShopController.cs
@@ -27,6 +27,24 @@ public class ShopController : MonoBehaviour
     GameObject _purchaseRevolverButtonObject;
     Button _purchaseRevolverButton;
 
+    GameObject _purchaseM4LaserButtonObject;
+    Button _purchaseM4LaserButton;
+
+    GameObject _purchaseSPAS12ButtonObject;
+    Button _purchaseSPAS12Button;
+
+    GameObject _purchaseM249ButtonObject;
+    Button _purchaseM249Button;
+
+    GameObject _purchaseSRLButtonObject;
+    Button _purchaseSRLButton;
+
+    GameObject _purchaseRPGButtonObject;
+    Button _purchaseRPGButton;
+
+    GameObject _purchaseRocketLauncherButtonObject;
+    Button _purchaseRocketLauncherButton;
+
     public GameObject BlurCanvasObject;
     Canvas BlurCanvas;
 
@@ -46,18 +64,36 @@ public class ShopController : MonoBehaviour
     public GameObject ImageScout;
     public GameObject ImageGoldenAK;
     public GameObject ImageRevolver;
+    public GameObject ImageM4Laser;
+    public GameObject ImageSPAS12;
+    public GameObject ImageM249;
+    public GameObject ImageSRL;
+    public GameObject ImageRPG;
+    public GameObject ImageRocketLauncher;
 
     public GameObject ConfirmedImageMP5;
     public GameObject ConfirmedImageShotgun;
     public GameObject ConfirmedImageScout;
     public GameObject ConfirmedImageGoldenAK;
     public GameObject ConfirmedImageRevolver;
+    public GameObject ConfirmedImageM4Laser;
+    public GameObject ConfirmedImageSPAS12;
+    public GameObject ConfirmedImageM249;
+    public GameObject ConfirmedImageSRL;
+    public GameObject ConfirmedImageRPG;
+    public GameObject ConfirmedImageRocketLauncher;
 
     public GameObject MP5OwnedDisplay;
     public GameObject ShotgunOwnedDisplay;
     public GameObject ScoutOwnedDisplay;
     public GameObject GoldenAKOwnedDisplay;
     public GameObject RevolverOwnedDisplay;
+    public GameObject M4LaserOwnedDisplay;
+    public GameObject SPAS12OwnedDisplay;
+    public GameObject M249OwnedDisplay;
+    public GameObject SRLOwnedDisplay;
+    public GameObject RPGOwnedDisplay;
+    public GameObject RocketLauncherOwnedDisplay;
 
 
     private string _selectedStoreWeapon;
@@ -86,27 +122,14 @@ public class ShopController : MonoBehaviour
         ConfirmationCanvas = ConfirmationCanvasObject.GetComponent<Canvas>();
         ConfirmationCanvas.enabled = false;
 
-        #endregion 
+        #endregion
 
         #region Images GameObjects
 
-        ImageMP5.SetActive(false);
-        ImageShotgun.SetActive(false);
-        ImageScout.SetActive(false);
-        ImageGoldenAK.SetActive(false);
-        ImageRevolver.SetActive(false);
+        disableAllImages();
+    
+        setOwnedTofalse();
 
-        ConfirmedImageMP5.SetActive(false);
-        ConfirmedImageShotgun.SetActive(false);
-        ConfirmedImageScout.SetActive(false);
-        ConfirmedImageGoldenAK.SetActive(false);
-        ConfirmedImageRevolver.SetActive(false);
-
-        MP5OwnedDisplay.SetActive(false);
-        ShotgunOwnedDisplay.SetActive(false);
-        ScoutOwnedDisplay.SetActive(false);
-        GoldenAKOwnedDisplay.SetActive(false);
-        RevolverOwnedDisplay.SetActive(false);
 
         #endregion
 
@@ -118,42 +141,16 @@ public class ShopController : MonoBehaviour
         #endregion
 
         #region Gun Buttons
-        //MP5
-        _purchaseMP5ButtonObject = GameObject.Find("PurchaseMP5Button");
-        _purchaseMP5Button = _purchaseMP5ButtonObject.GetComponent<Button>();
-        _purchaseMP5Button.onClick.AddListener(_purchaseMP5ButtonClicked);
-       
-
-        //Shotgun
-        _purchaseShotgunButtonObject = GameObject.Find("PurchaseShotgunButton");
-        _purchaseShotgunButton = _purchaseShotgunButtonObject.GetComponent<Button>();
-        _purchaseShotgunButton.onClick.AddListener(_purchaseShotgunButtonClicked);
-        
-
-        //GoldenAK
-        _purchaseGoldenAKButtonObject = GameObject.Find("PurchaseGoldenAKButton");
-        _purchaseGoldenAKButton = _purchaseGoldenAKButtonObject.GetComponent<Button>();
-        _purchaseGoldenAKButton.onClick.AddListener(_purchaseGoldenAKButtonClicked);
-        
-
-        //Scout
-        _purchaseScoutButtonObject = GameObject.Find("PurchaseScoutButton");
-        _purchaseScoutButton = _purchaseScoutButtonObject.GetComponent<Button>();
-        _purchaseScoutButton.onClick.AddListener(_purchaseScoutButtonClicked);
-
-
-        //Revolver
-        _purchaseRevolverButtonObject = GameObject.Find("PurchaseRevolverButton");
-        _purchaseRevolverButton = _purchaseRevolverButtonObject.GetComponent<Button>();
-        _purchaseRevolverButton.onClick.AddListener(_purchaseRevolverButtonClicked);
+        activatePage1Weapons();
 
         #endregion
 
-        updateShopButtons();
+        updateShopButtonsPage1();
         updateUserMoney();
     }
 
-    private void updateShopButtons()
+
+    private void updateShopButtonsPage1()
     {
         if (PlayerPrefs.HasKey("MP5"))
         {
@@ -200,6 +197,17 @@ public class ShopController : MonoBehaviour
             ScoutOwnedDisplay.SetActive(false);
         }
 
+        if (PlayerPrefs.HasKey("M4Laser"))
+        {
+            _purchaseM4LaserButton.enabled = false;
+            M4LaserOwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseM4LaserButton.enabled = true;
+            M4LaserOwnedDisplay.SetActive(false);
+        }
+
         if (PlayerPrefs.HasKey("Revolver"))
         {
             _purchaseRevolverButton.enabled = false;
@@ -211,6 +219,125 @@ public class ShopController : MonoBehaviour
             RevolverOwnedDisplay.SetActive(false);
         }
 
+        if (PlayerPrefs.HasKey("SPAS12"))
+        {
+            _purchaseSPAS12Button.enabled = false;
+            SPAS12OwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseSPAS12Button.enabled = true;
+            SPAS12OwnedDisplay.SetActive(false);
+        }
+
+        if (PlayerPrefs.HasKey("M249"))
+        {
+            _purchaseM249Button.enabled = false;
+            M249OwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseM249Button.enabled = true;
+            M249OwnedDisplay.SetActive(false);
+        }
+    }
+
+    public void updateShopButtonsPage2()
+    {
+        if (PlayerPrefs.HasKey("SRL"))
+        {
+            _purchaseSRLButton.enabled = false;
+            SRLOwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseSRLButton.enabled = true;
+            SRLOwnedDisplay.SetActive(false);
+        }
+
+        if (PlayerPrefs.HasKey("RPG"))
+        {
+            _purchaseRPGButton.enabled = false;
+            RPGOwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseRPGButton.enabled = true;
+            RPGOwnedDisplay.SetActive(false);
+        }
+
+        if (PlayerPrefs.HasKey("RocketLauncher"))
+        {
+            _purchaseRocketLauncherButton.enabled = false;
+            RocketLauncherOwnedDisplay.SetActive(true);
+        }
+        else
+        {
+            _purchaseRocketLauncherButton.enabled = true;
+            RocketLauncherOwnedDisplay.SetActive(false);
+        }
+    }
+
+    private void activatePage1Weapons()
+    {
+        //MP5
+        _purchaseMP5ButtonObject = GameObject.Find("PurchaseMP5Button");
+        _purchaseMP5Button = _purchaseMP5ButtonObject.GetComponent<Button>();
+        _purchaseMP5Button.onClick.AddListener(_purchaseMP5ButtonClicked);
+
+        //Shotgun
+        _purchaseShotgunButtonObject = GameObject.Find("PurchaseShotgunButton");
+        _purchaseShotgunButton = _purchaseShotgunButtonObject.GetComponent<Button>();
+        _purchaseShotgunButton.onClick.AddListener(_purchaseShotgunButtonClicked);
+
+        //GoldenAK
+        _purchaseGoldenAKButtonObject = GameObject.Find("PurchaseGoldenAKButton");
+        _purchaseGoldenAKButton = _purchaseGoldenAKButtonObject.GetComponent<Button>();
+        _purchaseGoldenAKButton.onClick.AddListener(_purchaseGoldenAKButtonClicked);
+
+        //Scout
+        _purchaseScoutButtonObject = GameObject.Find("PurchaseScoutButton");
+        _purchaseScoutButton = _purchaseScoutButtonObject.GetComponent<Button>();
+        _purchaseScoutButton.onClick.AddListener(_purchaseScoutButtonClicked);
+
+        //Revolver
+        _purchaseRevolverButtonObject = GameObject.Find("PurchaseRevolverButton");
+        _purchaseRevolverButton = _purchaseRevolverButtonObject.GetComponent<Button>();
+        _purchaseRevolverButton.onClick.AddListener(_purchaseRevolverButtonClicked);
+
+        //M4Lazer
+        _purchaseM4LaserButtonObject = GameObject.Find("PurchaseM4LaserButton");
+        _purchaseM4LaserButton = _purchaseM4LaserButtonObject.GetComponent<Button>();
+        _purchaseM4LaserButton.onClick.AddListener(_purchaseM4LaserButtonClicked);
+
+        //SPAS12
+        _purchaseSPAS12ButtonObject = GameObject.Find("PurchaseSPAS12Button");
+        _purchaseSPAS12Button = _purchaseSPAS12ButtonObject.GetComponent<Button>();
+        _purchaseSPAS12Button.onClick.AddListener(_purchaseSPAS12ButtonClicked);
+
+        //M429
+        _purchaseM249ButtonObject = GameObject.Find("PurchaseM249Button");
+        _purchaseM249Button = _purchaseM249ButtonObject.GetComponent<Button>();
+        _purchaseM249Button.onClick.AddListener(_purchasedM249ButtonClicked);
+
+    }
+
+    public void activatePage2Weapons()
+    {
+        //SRL
+        _purchaseSRLButtonObject = GameObject.Find("PurchaseSRLButton");
+        _purchaseSRLButton = _purchaseSRLButtonObject.GetComponent<Button>();
+        _purchaseSRLButton.onClick.AddListener(_purchasedSRLButtonClicked);
+
+        //RPG
+        _purchaseRPGButtonObject = GameObject.Find("PurchaseRPGButton");
+        _purchaseRPGButton = _purchaseRPGButtonObject.GetComponent<Button>();
+        _purchaseRPGButton.onClick.AddListener(_purchaseRPGButtonClicked);
+
+        //RocketLauncher
+        _purchaseRocketLauncherButtonObject = GameObject.Find("PurchaseRocketLauncherButton");
+        _purchaseRocketLauncherButton = _purchaseRocketLauncherButtonObject.GetComponent<Button>();
+        _purchaseRocketLauncherButton.onClick.AddListener(_purchaseRocketLauncherButtonClicked);
     }
 
     #region Purchase buttons 
@@ -258,6 +385,54 @@ public class ShopController : MonoBehaviour
         _selectedStoreWeapon = "Revolver";
     }
 
+    private void _purchaseRocketLauncherButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("RocketLauncher");
+        displayConfirmation(check, "RocketLauncher");
+        _selectedStoreWeapon = "RocketLauncher";
+    }
+
+    private void _purchaseRPGButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("RPG");
+        displayConfirmation(check, "RPG");
+        _selectedStoreWeapon = "RPG";
+    }
+
+    private void _purchasedSRLButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("SRL");
+        displayConfirmation(check, "SRL");
+        _selectedStoreWeapon = "SRL";
+    }
+
+    private void _purchasedM249ButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("M249");
+        displayConfirmation(check, "M249");
+        _selectedStoreWeapon = "M249";
+    }
+
+    private void _purchaseSPAS12ButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("SPAS12");
+        displayConfirmation(check, "SPAS12");
+        _selectedStoreWeapon = "SPAS12";
+    }
+
+    private void _purchaseM4LaserButtonClicked()
+    {
+        InitiateScreenFocus();
+        var check = CheckForEnoughCoins("M4Laser");
+        displayConfirmation(check, "M4Laser");
+        _selectedStoreWeapon = "M4Laser";
+    }
+
     #endregion
 
     public void _purchaseConfirmButtonClicked()
@@ -290,6 +465,36 @@ public class ShopController : MonoBehaviour
             ConfirmedImageRevolver.SetActive(true);
             UserProfile.decreaseCoins(150);
         }
+        else if (_selectedStoreWeapon.Equals("M4Laser"))
+        {
+            ConfirmedImageM4Laser.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
+        else if (_selectedStoreWeapon.Equals("SPAS12"))
+        {
+            ConfirmedImageSPAS12.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
+        else if (_selectedStoreWeapon.Equals("M249"))
+        {
+            ConfirmedImageM249.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
+        else if (_selectedStoreWeapon.Equals("SRL"))
+        {
+            ConfirmedImageSRL.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
+        else if (_selectedStoreWeapon.Equals("RPG"))
+        {
+            ConfirmedImageRPG.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
+        else if (_selectedStoreWeapon.Equals("RocketLauncher"))
+        {
+            ConfirmedImageRocketLauncher.SetActive(true);
+            UserProfile.decreaseCoins(150);
+        }
         PurchasedCanvas.enabled = true;
         updateUserMoney();
         _selectedStoreWeapon = "";
@@ -311,7 +516,8 @@ public class ShopController : MonoBehaviour
         PurchasedCanvas.enabled = false;
         BlurCanvas.enabled = false;
         disableAllImages();
-        updateShopButtons();
+        updateShopButtonsPage1();
+        updateShopButtonsPage2();
         _selectedStoreWeapon = "";
     }
 
@@ -324,17 +530,44 @@ public class ShopController : MonoBehaviour
 
     public void disableAllImages()
     {
-        ConfirmedImageMP5.SetActive(false);
-        ConfirmedImageGoldenAK.SetActive(false);
-        ConfirmedImageScout.SetActive(false);
-        ConfirmedImageShotgun.SetActive(false);
-        ConfirmedImageRevolver.SetActive(false);
-
-        ImageGoldenAK.SetActive(false);
-        ImageScout.SetActive(false);
         ImageMP5.SetActive(false);
         ImageShotgun.SetActive(false);
+        ImageScout.SetActive(false);
+        ImageGoldenAK.SetActive(false);
         ImageRevolver.SetActive(false);
+        ImageM4Laser.SetActive(false);
+        ImageSPAS12.SetActive(false);
+        ImageM249.SetActive(false);
+        ImageSRL.SetActive(false);
+        ImageRPG.SetActive(false);
+        ImageRocketLauncher.SetActive(false);
+
+        ConfirmedImageMP5.SetActive(false);
+        ConfirmedImageShotgun.SetActive(false);
+        ConfirmedImageScout.SetActive(false);
+        ConfirmedImageGoldenAK.SetActive(false);
+        ConfirmedImageRevolver.SetActive(false);
+        ConfirmedImageM4Laser.SetActive(false);
+        ConfirmedImageSPAS12.SetActive(false);
+        ConfirmedImageM249.SetActive(false);
+        ConfirmedImageSRL.SetActive(false);
+        ConfirmedImageRPG.SetActive(false);
+        ConfirmedImageRocketLauncher.SetActive(false);
+    }
+
+    void setOwnedTofalse()
+    {
+        MP5OwnedDisplay.SetActive(false);
+        ShotgunOwnedDisplay.SetActive(false);
+        ScoutOwnedDisplay.SetActive(false);
+        GoldenAKOwnedDisplay.SetActive(false);
+        RevolverOwnedDisplay.SetActive(false);
+        M4LaserOwnedDisplay.SetActive(false);
+        SPAS12OwnedDisplay.SetActive(false);
+        M249OwnedDisplay.SetActive(false);
+        SRLOwnedDisplay.SetActive(false);
+        RPGOwnedDisplay.SetActive(false);
+        RocketLauncherOwnedDisplay.SetActive(false);
     }
 
     #endregion
@@ -369,6 +602,36 @@ public class ShopController : MonoBehaviour
             else if (weapon.Equals("Revolver"))
             {
                 ImageRevolver.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("M4Laser"))
+            {
+                ImageM4Laser.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("SPAS12"))
+            {
+                ImageSPAS12.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("M249"))
+            {
+                ImageM249.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("SRL"))
+            {
+                ImageSRL.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("RPG"))
+            {
+                ImageRPG.SetActive(true);
+                Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
+            }
+            else if (weapon.Equals("RocketLauncher"))
+            {
+                ImageRocketLauncher.SetActive(true);
                 Purchase_Text.GetComponentInChildren<Text>().text = "Current balance: " + UserProfile.getCoins() + System.Environment.NewLine + "Cost: 150" + System.Environment.NewLine + "New Balance: " + (UserProfile.getCoins() - 150);
             }
 
@@ -411,6 +674,48 @@ public class ShopController : MonoBehaviour
             }
         }
         else if (_selectedGun.Equals("Revolver"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("M4Laser"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("SPAS12"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("M249"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("SRL"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("RPG"))
+        {
+            if (2000 >= 300)
+            {
+                return true;
+            }
+        }
+        else if (_selectedGun.Equals("RocketLauncher"))
         {
             if (2000 >= 300)
             {
